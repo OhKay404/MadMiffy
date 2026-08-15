18d76f7f3081716d92c18488a69b4ef1606a7e19
OhKay404 <kayleechan0322@gmail.com>
2026-08-15 12:39:00 -0400

    Updated the script of the sprite so that the sprite horizontally flips depending on which arrow key was pressed

--- Scripts/player_scene.gd
+++ Scripts/player_scene.gd
@@ -16,10 +16,12 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		$Sprite2D.flip_h = true
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		$Sprite2D.flip_h = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
 	move_and_slide()
