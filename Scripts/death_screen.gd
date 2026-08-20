c86e79b1237b4d66fc4ef64d281f177dd2a17382
OhKay404 <kayleechan0322@gmail.com>
2026-08-20 16:00:55 -0400

    Added game directions to minigame 2 and updated winner and death screen scripts
    Global.lives and Global.minigames_done is reset to 0 when the user clicks "Main Menu" button in death and winner screens

--- Scripts/death_screen.gd
+++ Scripts/death_screen.gd
@@ -13,3 +13,5 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
+	Global.lives = 5
+	Global.minigames_done = 0
