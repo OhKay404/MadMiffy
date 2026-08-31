861646a9c66a99c702fb3b481886d1573efbd208
OhKay404 <kayleechan0322@gmail.com>
2026-08-30 21:52:46 -0400

    Add script to settings screen and ToggleVolume CheckButton for button functionality

--- Scripts/settings_scene.gd
+++ Scripts/settings_scene.gd
@@ -5,7 +5,6 @@ extends Node2D
 func _ready() -> void:
 	$VolumeAdjust.value = ($VolumeAdjust.min_value + $VolumeAdjust.max_value) / 2
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -13,3 +12,13 @@ func _process(delta: float) -> void:
 
 func _on_volume_adjust_value_changed(value) -> void:
 	AudioServer.set_bus_volume_db(0, value)
+
+
+
+func _on_toggle_volume_toggled(toggled_on: bool) -> void:
+	if !toggled_on:
+		$Test.stream_paused = true
+		$VolumeOnText.text = "Off"
+	elif toggled_on:
+		$Test.stream_paused = false
+		$VolumeOnText.text = "On"
