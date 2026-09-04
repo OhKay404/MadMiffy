c27b0a070b984f1e674ba4de65fdd209f189067a
OhKay404 <kayleechan0322@gmail.com>
2026-09-04 14:27:22 -0400

    Add click animation to winner screen main menu button

---
+++ Scripts/win_button.gd
@@ -0,0 +1,28 @@
+extends Button
+
+var tween:Tween
+var original_Y = position.y
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pivot_offset = size * Vector2(0.5, 1.0)
+	button_down.connect(_on_button_down)
+	button_up.connect(_on_button_up)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_button_down() -> void:
+	if tween: tween.kill()
+	scale = Vector2(0.9, 0.7)
+
+
+
+func _on_button_up() -> void:
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.1)
