79fd0f9b5139ef0c0a0de0a2cc231e03bd229c3f
OhKay404 <kayleechan0322@gmail.com>
2026-09-08 23:09:33 -0400

    Add script to BubbleFood TextureRect so that a random food spawns inside of the text bubble when the screen loads, Add themed timer to minigame_3

--- Scripts/minigame_3.gd
+++ Scripts/minigame_3.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var mouse_position:Vector2 = get_global_mouse_position()
+@onready var themed_timer: Node2D = $ThemedTimer
 @onready var food1:TextureRect = $Food1
 @onready var food2:TextureRect = $Food2
 @onready var food3:TextureRect = $Food3
@@ -8,7 +9,7 @@ var mouse_position:Vector2 = get_global_mouse_position()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	await themed_timer.Timer(8.5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
