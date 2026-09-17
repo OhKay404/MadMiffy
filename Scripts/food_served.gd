bfcce3076b7ce08331ceeb866b65162c1e5d52d6
OhKay404 <kayleechan0322@gmail.com>
2026-09-16 22:07:31 -0400

    Initialize tween animation for food_served RichTextLabel

--- Scripts/food_served.gd
+++ Scripts/food_served.gd
@@ -1,11 +1,20 @@
 extends RichTextLabel
 
+var tween:Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	floating()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	text = "Food Served: " + str(get_parent().num_clicked) + "/3"
+
+func floating() -> void:
+	if tween:tween.kill
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "position:y", position.y + 5, 0.1)
+	tween.tween_property(self, "position:y", position.y - 5, 0.1)
