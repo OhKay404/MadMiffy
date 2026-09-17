1cf4c4821b5080418e7a6270b8001313f2139241
OhKay404 <kayleechan0322@gmail.com>
2026-09-16 21:50:52 -0400

    Attach script to food_served RichTextLabel to update the number of times the player selected the correct option in minigame_3

--- Scripts/food_served.gd
+++ Scripts/food_served.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	text = "Food Served: " + str(get_parent().num_clicked) + "/3"
