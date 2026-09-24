d3075a7ddd96d008fa201a2ff8e5cf7defaf0409
OhKay404 <kayleechan0322@gmail.com>
2026-09-23 21:49:25 -0400

    Create new scripts for each food button in minigame_3 and add tween hover state script for each button

---
+++ Scripts/food_1.gd
@@ -0,0 +1,29 @@
+extends TextureButton
+
+var tween:Tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_mouse_entered() -> void:
+	if tween:tween.kill()
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "scale", Vector2(0.28, 0.28), 0.1)
+
+
+func _on_mouse_exited() -> void:
+	if tween:tween.kill()
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "scale", Vector2(0.23, 0.23), 0.1)
--- Scripts/minigame_3.gd
+++ Scripts/minigame_3.gd
@@ -35,8 +35,6 @@ func _process(delta: float) -> void:
 			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
 
 
-
-
 func _on_food_1_pressed() -> void:
 	if bubble1.modulate == Color("#ffffff"):
 		bubble1.modulate = Color("#ffffff00")
@@ -60,35 +58,6 @@ func _on_food_1_pressed() -> void:
 			get_tree().change_scene_to_file("res://Scenes/death_screen.tscn")
 		else:
 			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-
-	
-	
-	#if bubble_food.texture.resource_path == "res://Images/Food1.png":
-		#num_clicked += 1
-		#if num_clicked == 3:
-			#if Global.minigames_done == 3:
-				#get_tree().change_scene_to_file("res://Scenes/winner_screen.tscn")
-			#else:
-				#get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-		#else:
-			#randomize()
-			#var random_num = randi_range(1, 4)
-			#bubble_food.texture = load("res://Images/Food" + str(int(random_num)) + ".png")
-			#modulate = "#ffffff00"
-			#await get_tree().create_timer(0.3).timeout
-			#modulate = "#ffffff"
-	#else:
-		#Global.lives -= 1
-		#Global.minigames_done -=1
-		#if Global.lives < 1:
-			#get_tree().change_scene_to_file("res://Scenes/death_screen.tscn")
-		#else:
-			#get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-			#var random_num = int(randi_range(1, 4))
-			#var selected_bubble = get("bubble" + str(random_num))
-			#modulate = "#ffffff00"
-			#await get_tree().create_timer(0.3).timeout
-			#selected_bubble.modulate = Color("#ffffff")
 
 
 func _on_food_2_pressed() -> void:
@@ -114,27 +83,7 @@ func _on_food_2_pressed() -> void:
 			get_tree().change_scene_to_file("res://Scenes/death_screen.tscn")
 		else:
 			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-	#if bubble_food.texture.resource_path == "res://Images/Food2.png":
-		#num_clicked += 1
-		#if num_clicked == 3:
-			#if Global.minigames_done == 3:
-				#get_tree().change_scene_to_file("res://Scenes/winner_screen.tscn")
-			#else:
-				#get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-		#else:
-			#randomize()
-			#var random_num = randi_range(1, 4)
-			#bubble_food.texture = load("res://Images/Food" + str(int(random_num)) + ".png")
-			#modulate = "#ffffff00"
-			#await get_tree().create_timer(0.3).timeout
-			#modulate = "#ffffff"
-	#else:
-		#Global.lives -= 1
-		#Global.minigames_done -=1
-		#if Global.lives < 1:
-			#get_tree().change_scene_to_file("res://Scenes/death_screen.tscn")
-		#else:
-			#get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
+
 
 func _on_food_3_pressed() -> void:
 	if bubble3.modulate == Color("#ffffff"):
@@ -159,27 +108,7 @@ func _on_food_3_pressed() -> void:
 			get_tree().change_scene_to_file("res://Scenes/death_screen.tscn")
 		else:
 			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-	#if bubble_food.texture.resource_path == "res://Images/Food3.png":
-		#num_clicked += 1
-		#if num_clicked == 3:
-			#if Global.minigames_done == 3:
-				#get_tree().change_scene_to_file("res://Scenes/winner_screen.tscn")
-			#else:
-				#get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-		#else:
-			#randomize()
-			#var random_num = randi_range(1, 4)
-			#bubble_food.texture = load("res://Images/Food" + str(int(random_num)) + ".png")
-			#modulate = "#ffffff00"
-			#await get_tree().create_timer(0.3).timeout
-			#modulate = "#ffffff"
-	#else:
-		#Global.lives -= 1
-		#Global.minigames_done -=1
-		#if Global.lives < 1:
-			#get_tree().change_scene_to_file("res://Scenes/death_screen.tscn")
-		#else:
-			#get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
+
 
 func _on_food_4_pressed() -> void:
 	if bubble4.modulate == Color("#ffffff"):
@@ -204,24 +133,3 @@ func _on_food_4_pressed() -> void:
 			get_tree().change_scene_to_file("res://Scenes/death_screen.tscn")
 		else:
 			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-	#if bubble_food.texture.resource_path == "res://Images/Food4.png":
-		#num_clicked += 1
-		#if num_clicked == 3:
-			#if Global.minigames_done == 3:
-				#get_tree().change_scene_to_file("res://Scenes/winner_screen.tscn")
-			#else:
-				#get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
-		#else:
-			#randomize()
-			#var random_num = randi_range(1, 4)
-			#bubble_food.texture = load("res://Images/Food" + str(int(random_num)) + ".png")
-			#modulate = "#ffffff00"
-			#await get_tree().create_timer(0.3).timeout
-			#modulate = "#ffffff"
-	#else:
-		#Global.lives -= 1
-		#Global.minigames_done -=1
-		#if Global.lives < 1:
-			#get_tree().change_scene_to_file("res://Scenes/death_screen.tscn")
-		#else:
-			#get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
